e67d14529fb79538deb7613e0e5b5ecb81c195c3
x280 <86424319+0x280@users.noreply.github.com>
2022-04-14 18:48:59 +0200

    initial commit

---
+++ src/dumper.ts
@@ -0,0 +1,97 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import axios from 'axios';
+
+export const CONFIG = {
+    DUMP_PATH: process.env.INIT_CWD + '/dump',
+    ITEM_ITERATION: 500,
+    COLOR_ITERATION: 50
+};
+
+const CLOTHES_NAME_MAP = new Map([
+    [ 1, 'masks' ],
+    [ 4, 'pants' ],
+    [ 5, 'backpacks' ],
+    [ 6, 'shoes' ],
+    [ 7, 'accessories' ],
+    [ 8, 'undershirts' ],
+    [ 9, 'vests' ],
+    [ 11, 'tops' ],
+    [ 12, 'hats' ],
+    [ 13, 'glasses' ],
+    [ 14, 'watches' ],
+    [ 15, 'earpieces' ],
+    [ 16, 'gloves' ]
+]);
+
+async function dumpFile(url: string, filePath: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const dirname = path.dirname(filePath);
+        fs.mkdir(dirname, {recursive: true}, (err) => {
+            axios({ url: url, method: 'GET', responseType: 'stream' })
+                .catch(reason => {
+                    if (reason && reason.response && reason.response.status == 404) {
+                        resolve();
+                    } else resolve();
+                })
+                .then(async res => {
+                    if (res) {
+                        const writeStream = fs.createWriteStream(filePath);
+                        res.data.pipe(writeStream);
+
+                        writeStream.on('finish', resolve);
+                        writeStream.on('error', reject);
+                    }
+                }
+            );
+        })
+        
+    })
+}
+
+export async function dumpClothes(isMale: boolean = true): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const gender = (isMale ? 'male' : 'female');
+
+        CLOTHES_NAME_MAP.forEach(async (name, id) => {
+            for (let i = 0; i < CONFIG.ITEM_ITERATION; i++) {
+                for (let n = 0; n < CONFIG.COLOR_ITERATION; n++) {
+                    await dumpFile(`https://launcher.gta5grand.com/game/images/${gender}/${id}/${i}_${n}.png`, `${CONFIG.DUMP_PATH}\\clothes\\${gender}\\${name}\\${i}_${n}.png`)
+                        .catch(console.error);
+                    console.log('dumped: '+ `https://launcher.gta5grand.com/game/images/${gender}/${id}/${i}_${n}.png`)
+                }
+            }
+        })
+
+        resolve();
+    })
+}
+
+export async function dumpOtherItems() {
+    return new Promise<void>(async (resolve, reject) => {
+        for (let i = 0; i < CONFIG.ITEM_ITERATION; i++) {
+            await dumpFile(`https://launcher.gta5grand.com/game/images/other_items/${i}.png`, `${CONFIG.DUMP_PATH}\\other_items\\${i}.png`)
+                    .catch(reject);
+            console.log('dumped: '+ `https://launcher.gta5grand.com/game/images/other_items/${i}.png`);
+        }
+        resolve();
+    })
+}
+
+export async function clearPreviousDump(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        fs.rm(CONFIG.DUMP_PATH, {recursive: true}, (err) => {
+            if (err && err.errno != -4058) {
+                reject(err);
+            } else {
+                fs.mkdir(CONFIG.DUMP_PATH, (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                })
+            }
+        });
+    });
+}
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import * as dumper from './dumper'
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+import { exit } from 'process';
+
+(
+    async () => {
+        if (isMainThread) {
+            await dumper.clearPreviousDump().catch(console.error);
+            const workers = [
+                new Worker(__filename, { workerData: 0 }),
+                new Worker(__filename, { workerData: 1 }),
+                new Worker(__filename, { workerData: 2 })
+            ];
+            let finishCount = 0;
+            workers.forEach(w => {
+                w.on('exit', (code) => {
+                    if (code !== 0)
+                      console.error(new Error(`Worker stopped with exit code ${code}`));
+                    finishCount++;
+                });
+            });
+
+            setInterval(() => {
+                if (finishCount >= 3) {
+                    console.log('finished');
+                    exit(0);
+                }
+            }, 1000)
+        } else {
+            switch (workerData) {
+                case 0:
+                    await dumper.dumpOtherItems();
+                    break;
+                case 1:
+                    await dumper.dumpClothes(true);
+                    break;
+                case 2:
+                    await dumper.dumpClothes(false);
+                    break;
+                default:
+                    break;
+            } 
+        }
+    }
+)();
